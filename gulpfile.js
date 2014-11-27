05b25818670c83b7787d5d999ee5da905d2aa83f
yodairish <yodairish@gmail.com>
2014-11-27 15:18:54 +0000

    add main workflow files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
+'use strict';
+
 var gulp = require('gulp'),
+    merge = require('merge-stream'),
+    pf = require('./js/paramFiles'),
     sourcemaps = require('gulp-sourcemaps'),
     // css
     csscomb = require('gulp-csscomb'),
@@ -7,92 +11,96 @@ var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     // js
     sass = require('gulp-sass'),
-    jshint = require("gulp-jshint"),
-    jscs = require("gulp-jscs"),
-    webpack = require('gulp-webpack');
+    jshint = require('gulp-jshint'),
+    jscs = require('gulp-jscs'),
+    webpack = require('gulp-webpack'),
+    // CONSTANTS
+    SCSS_PATH = 'css',
+    JS_PATH = 'js',
+    COMPONENT_PATH = 'components',
+    PUBLIC_CSS = 'public/css',
+    PUBLIC_JS = 'public/js',
+    JS_ENTRY_POINTS = [
+      JS_PATH + '/main.js'
+    ];
 
+/**
+ * Linting SCSS files
+ * Possible send files to param -files=...
+ */
 gulp.task('csslint', function() {
-  var files = getChosenFiles(process.argv);
+  var files = pf.paramFiles(),
+      stream,
+      global,
+      components;
 
-  if (!files) {
-    files = ['css/**/*.scss'];
+  if (files) {
+    stream = gulp.src(files)
+      .pipe(csscomb(__dirname + '/.csscomb.json'))
+      .pipe(gulp.dest('./'));
+
+  } else {
+    global = gulp.src(pf.scss(SCSS_PATH))
+      .pipe(csscomb(__dirname + '/.csscomb.json'))
+      .pipe(gulp.dest(SCSS_PATH));
+
+    components = gulp.src(pf.scss(COMPONENT_PATH))
+      .pipe(csscomb(__dirname + '/.csscomb.json'))
+      .pipe(gulp.dest(COMPONENT_PATH));
+
+    stream = merge(global, components);
   }
-  
-  return gulp.src(files)
-    .pipe(csscomb(__dirname + '/.csscomb.json'))
-    .pipe(gulp.dest('css'));
+
+  return stream;
 });
 
+/**
+ * Processing SCSS files
+ */
 gulp.task('css', ['csslint'], function() {
   var processors = [
     autoprefixer({
-      browsers: ['last 2 version']
+      browsers: ['last 1 version']
     }),
     csswring({
       removeAllComments: true
     })
   ];
-  
-  return gulp.src(['css/components/**/*.scss'])
+
+  return gulp.src([pf.scss(SCSS_PATH)])
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(postcss(processors))
-    .pipe(sourcemaps.write('public/css/map'))
-    .pipe(gulp.dest('public/css'));
+    .pipe(sourcemaps.write(PUBLIC_CSS + '/map'))
+    .pipe(gulp.dest(PUBLIC_CSS));
 });
 
+/**
+ * Linting JS files
+ * Possible send files to param -files=...
+ */
 gulp.task('jslint', function() {
-  var files = getChosenFiles(process.argv);
+  var files = pf.paramFiles();
 
   if (!files) {
-    files = ['js/**/*.js'];
+    files = [pf.js(JS_PATH), pf.js(COMPONENT_PATH)];
   }
-  
+
   return gulp.src(files)
-    .pipe(gulp.dest('client/js/hint/result'))
     .pipe(jshint.extract())
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('fail'))
-    .pipe(jscs({
-      "preset": "airbnb"
-    }));
+    .pipe(jscs())
+    .pipe(jshint.reporter('fail'));
 });
 
+/**
+ * Processing JS files
+ */
 gulp.task('js', ['jslint'], function() {
-  return gulp.src(['js/components/**/*.js'])
-    .pipe(webpack({
-      
-    }))
-    .pipe(gulp.dest('public/css'));
+  return gulp.src(JS_ENTRY_POINTS)
+    .pipe(webpack())
+    .pipe(gulp.dest(PUBLIC_JS));
 });
 
 gulp.task('default', ['js', 'css']);
-
-/**
- * Tries to get the files sent as an argument
- * @param {Array.<string>}
- * @return {?Array}
- */
-function getChosenFiles(argv) {
-  var chosen = false,
-      files = [];
-  
-  argv.forEach(function(arg) {
-    if (arg.indexOf('-files') === 0 || arg.indexOf('-file') === 0) {
-      var value = arg.split('=')[1];
-      
-      if (value) {
-        value.split(',').forEach(function(file) {
-          if (file) {
-            files.push(file);
-          }
-        });
-      }
-      
-      chosen = true;
-    }
-  });
-  
-  return (chosen ? files : null);
-}
